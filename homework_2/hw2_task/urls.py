e2dc1f1cdc5406aa34bb904d5a19d4826ea6fe07
Gordey <gordeymetelkov@icloud.com>
2024-04-03 00:13:52 +0300

    6.2

--- homework_2/hw2_task/urls.py
+++ homework_2/hw2_task/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('orders_by_client/<int:client_id>', views.get_all_orders_by_client, name='orders_by_client'),
     path('orders_by_time/<int:client_id>/<int:time>', views.get_orders_by_time, name='orders_by_time'),
     path('product/<int:product_id>/', views.get_product, name='get_product'),
-    path('edit_product/<int:product_id>/', views.edit_product, name= 'edit_product'),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('', views.index, name='index')
 ]
