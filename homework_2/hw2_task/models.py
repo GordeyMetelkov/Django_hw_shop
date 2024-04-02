0ffdfb5bf70a74e8e0c6a7fc4f0e19866491a999
Gordey <gordeymetelkov@icloud.com>
2024-04-02 23:08:02 +0300

    hw

--- homework_2/hw2_task/models.py
+++ homework_2/hw2_task/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     product_count = models.IntegerField(default=0)
     additions_date = models.DateField(auto_now_add=True)
-    image = models.ImageField(default='')
+    image = models.ImageField(default='', width_field=250, height_field=250)
 
 
 class Order(models.Model):
